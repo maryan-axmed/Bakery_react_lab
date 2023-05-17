853cc85ba8c5248d91e2dad9e8e08389c5d34e70
maryan axmed <12maxmed@gmail.com>
2023-05-17 18:23:30 +0100

    intial commit

---
+++ src/cake/Cake.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import CakeContainer from "./CakeContainer";
+
+const Cake = ({cake}) => {
+
+  const addIngredients = cake.ingredients.map(function(item){
+    return <li>{item}</li> ;
+  });
+
+
+  return (
+    <>
+        <h2>{cake.cakeName}</h2>
+        <p>Ingredients List:</p>
+          <ul>
+            {addIngredients}
+          </ul>
+        <p>Price: {cake.price}</p>
+        <p>Rating: {cake.rating}</p>
+    </>
+  )
+};
+
+export default Cake;
